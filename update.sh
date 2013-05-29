65368daa236c570c2d9188b22c115ca12cfb0a33
balemn <enriquev.rodriguez@gmail.com>
2013-05-28 21:12:42 -0400

    noesactualizacion

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ git pull https://github.com/UCSA-JAVA/SISALE.git master
 git add *
 echo 'Introduzca un comentario para el commit, debe ser distinto a ACTUALIZACION'
 read var
-git commit -m "'".$var."''"
+git commit -m ""$var""
 git push -u origin master
